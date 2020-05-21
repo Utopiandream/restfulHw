e5c605daae706dd47d40b952873f6aa644d80233
Utopiandream <utopiandream247@yahoo.com>
2020-05-21 17:14:28 -0500

    Added Basic Spring Boot Start project

---
+++ src/main/java/com/charter/restfulHw/RestfulHwApplication.java
@@ -0,0 +1,13 @@
+package com.charter.restfulHw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charter/restfulHw/RestfulHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.charter.restfulHw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
