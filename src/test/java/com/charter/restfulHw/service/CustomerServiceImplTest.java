b87e3742cbb67789fb896b59bcdc7ea984eec76d
Utopiandream <utopiandream247@yahoo.com>
2020-05-23 07:50:44 -0500

    Removed Logging from Tests in build

--- src/test/java/com/charter/restfulHw/service/CustomerServiceImplTest.java
+++ src/test/java/com/charter/restfulHw/service/CustomerServiceImplTest.java
@@ -17,8 +17,10 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
+@ActiveProfiles(profiles = "unitTest")
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class CustomerServiceImplTest {
